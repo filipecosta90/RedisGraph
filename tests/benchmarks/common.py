3579f221fc1058a94af6bce1c3a79f9586620763
filipecosta90 <filipecosta.90@gmail.com>
2021-02-02 15:00:22 +0000

    [wip] Further improved remote script

--- tests/benchmarks/common.py
+++ tests/benchmarks/common.py
@@ -1,8 +1,9 @@
 import logging
+import os
 import sys
 import pysftp
 import paramiko
-
+from git import Repo
 
 def viewBar_no_tqdm(a, b):
     res = a / int(b) * 100
@@ -57,3 +58,172 @@ def executeRemoteCommands(server_public_ip, username, private_key, commands):
         res.append([stdout,stderr])
     c.close()
     return res
+
+
+def checkDatasetRemoteRequirements(
+    benchmark_config, server_public_ip, username, private_key, remote_dataset_file
+):
+    for k in benchmark_config["dbconfig"]:
+        if "dataset" in k:
+            dataset = k["dataset"]
+    if dataset is not None:
+        copyFileToRemoteSetup(
+            server_public_ip,
+            username,
+            private_key,
+            dataset,
+            remote_dataset_file,
+        )
+
+
+def setupRemoteEnviroment(
+    tf, tf_github_sha, tf_github_actor, tf_setup_name, tf_github_repo, tf_redis_module
+):
+    return_code, stdout, stderr = tf.init(capture_output=True)
+    return_code, stdout, stderr = tf.refresh()
+    tf_output = tf.output()
+    server_private_ip = tf_output["server_private_ip"]["value"][0]
+    server_public_ip = tf_output["server_public_ip"]["value"][0]
+    client_private_ip = tf_output["client_private_ip"]["value"][0]
+    client_public_ip = tf_output["client_public_ip"]["value"][0]
+    if (
+        server_private_ip is not None
+        or server_public_ip is not None
+        or client_private_ip is not None
+        or client_public_ip is not None
+    ):
+        logging.warning("Destroying previous setup")
+        tf.destroy()
+    return_code, stdout, stderr = tf.apply(
+        skip_plan=True,
+        capture_output=False,
+        refresh=True,
+        var={
+            "github_sha": tf_github_sha,
+            "github_actor": tf_github_actor,
+            "setup_name": tf_setup_name,
+            "github_repo": tf_github_repo,
+            "redis_module": tf_redis_module,
+        },
+    )
+    tf_output = tf.output()
+    server_private_ip = tf_output["server_private_ip"]["value"][0]
+    server_public_ip = tf_output["server_public_ip"]["value"][0]
+    server_plaintext_port = 6379
+    client_private_ip = tf_output["client_private_ip"]["value"][0]
+    client_public_ip = tf_output["client_public_ip"]["value"][0]
+    username = "ubuntu"
+    return (
+        return_code,
+        username,
+        server_private_ip,
+        server_public_ip,
+        server_plaintext_port,
+        client_private_ip,
+        client_public_ip,
+    )
+
+
+def prepareBenchmarkCommand(
+    server_private_ip, server_plaintext_port, benchmark_config, results_file
+):
+    queries_str = ""
+    for k in benchmark_config["queries"]:
+        query = k["q"]
+        queries_str += ' -query "{}"'.format(query)
+        if "ratio" in k:
+            queries_str += " -query-ratio {}".format(k["ratio"])
+    for k in benchmark_config["clientconfig"]:
+        if "graph" in k:
+            queries_str += " -graph-key {}".format(k["graph"])
+        if "clients" in k:
+            queries_str += " -c {}".format(k["clients"])
+        if "requests" in k:
+            queries_str += " -n {}".format(k["requests"])
+        if "rps" in k:
+            queries_str += " -rps {}".format(k["rps"])
+    queries_str += " -h {}".format(server_private_ip)
+    queries_str += " -p {}".format(server_plaintext_port)
+
+    queries_str += " -json-out-file {}".format(results_file)
+    logging.info(
+        "Running the benchmark with the following parameters: {}".format(queries_str)
+    )
+    return queries_str
+
+
+def spinUpRemoteRedis(
+    benchmark_config,
+    server_public_ip,
+    username,
+    private_key,
+    local_module_file,
+    remote_module_file,
+    remote_dataset_file,
+):
+
+    # copy the rdb to DB machine
+    dataset = None
+    checkDatasetRemoteRequirements(
+        benchmark_config,
+        server_public_ip,
+        username,
+        private_key,
+        remote_dataset_file,
+    )
+
+    # copy the module to the DB machine
+    copyFileToRemoteSetup(
+        server_public_ip,
+        username,
+        private_key,
+        local_module_file,
+        remote_module_file,
+    )
+    executeRemoteCommands(
+        server_public_ip,
+        username,
+        private_key,
+        ["chmod 755 {}".format(remote_module_file)],
+    )
+    # start redis-server
+    commands = [
+        "redis-server --dir /tmp/ --daemonize yes --protected-mode no --loadmodule {}".format(
+            remote_module_file
+        )
+    ]
+    executeRemoteCommands(server_public_ip, username, private_key, commands)
+
+
+def setupRemoteBenchmark(
+    client_public_ip, username, private_key, redisbenchmark_go_link
+):
+    commands = [
+        "wget {} -q -O /tmp/redisgraph-benchmark-go".format(redisbenchmark_go_link),
+        "chmod 755 /tmp/redisgraph-benchmark-go",
+    ]
+    executeRemoteCommands(client_public_ip, username, private_key, commands)
+
+
+def runRemoteBenchmark(client_public_ip, username, private_key,server_private_ip, server_plaintext_port, benchmark_config, remote_results_file, local_results_file):
+    queries_str = prepareBenchmarkCommand(
+        server_private_ip, server_plaintext_port, benchmark_config, remote_results_file
+    )
+    commands = ["/tmp/redisgraph-benchmark-go {}".format(queries_str)]
+    executeRemoteCommands(client_public_ip, username, private_key, commands)
+    logging.info("Extracting the benchmark results")
+    getFileFromRemoteSetup(
+        client_public_ip,
+        username,
+        private_key,
+        local_results_file,
+        remote_results_file,
+    )
+
+
+def extract_git_vars():
+    github_repo = Repo("{}/../..".format(os.getcwd()))
+    github_repo_name = github_repo.remotes[0].config_reader.get("url")
+    github_sha = github_repo.head.object.hexsha
+    github_actor = github_repo.config_reader().get_value("user", "name")
+    return github_repo_name, github_sha, github_actor
--- tests/benchmarks/remote-runner.py
+++ tests/benchmarks/remote-runner.py
@@ -2,17 +2,32 @@ import yaml
 import pathlib
 from python_terraform import Terraform
 import argparse
-from git import Repo
 import os
 import logging
 
-from common import copyFileToRemoteSetup, executeRemoteCommands, getFileFromRemoteSetup
+from common import (
+    setupRemoteEnviroment,
+    spinUpRemoteRedis,
+    setupRemoteBenchmark,
+    runRemoteBenchmark,
+    extract_git_vars,
+)
 
-github_repo = Repo("{}/../..".format(os.getcwd()))
-github_repo_name = github_repo.remotes[0].config_reader.get("url")
-github_sha = github_repo.head.object.hexsha
-github_actor = github_repo.config_reader().get_value("user", "name")
+# logging settings
+logging.basicConfig(    
+    format="%(asctime)s %(levelname)-4s %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+# internal aux vars
+github_repo_name, github_sha, github_actor = extract_git_vars()
 redisbenchmark_go_link = "https://s3.amazonaws.com/benchmarks.redislabs/redisgraph/redisgraph-benchmark-go/unstable/redisgraph-benchmark-go_linux_amd64"
+private_key = "/home/filipe/.ssh/perf-cto-joint-tasks.pem"
+remote_dataset_file = "/tmp/dump.rdb"
+remote_module_file = "/tmp/redisgraph.so"
+local_results_file = "./benchmark-result.json"
+remote_results_file = "/tmp/benchmark-result.json"
 
 parser = argparse.ArgumentParser(
     description="RedisGraph remote performance tester.",
@@ -33,11 +48,18 @@ tf_redis_module = args.redis_module
 tf_setup_name_sufix = "{}-{}".format(args.setup_name_sufix, tf_github_sha)
 local_module_file = args.module_path
 
-logging.basicConfig(
-    format="%(asctime)s %(levelname)-8s %(message)s",
-    level=logging.INFO,
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
+if os.getenv("EC2_ACCESS_KEY", None) is None:
+    logging.error("missing required EC2_ACCESS_KEY env variable")
+    exit(1)
+if os.getenv("EC2_PRIVATE_PEM", None) is None:
+    logging.error("missing required EC2_PRIVATE_PEM env variable")
+    exit(1)
+if os.getenv("EC2_REGION", None) is None:
+    logging.error("missing required EC2_REGION env variable")
+    exit(1)
+if os.getenv("EC2_SECRET_KEY", None) is None:
+    logging.error("missing required EC2_SECRET_KEY env variable")
+    exit(1)
 
 logging.info("Using the following vars on terraform deployment:")
 logging.info("\tgithub_actor: {}".format(tf_github_actor))
@@ -53,135 +75,54 @@ remote_benchmark_setups = pathlib.Path().glob("./aws/tf-*")
 for f in files:
     with open(f, "r") as stream:
         benchmark_config = yaml.safe_load(stream)
-        for remote_setup in benchmark_config["ci"]["terraform"]:
-            logging.info(
-                "Deploying test defined in {} on AWS using {}".format(f, remote_setup)
-            )
-            tf_setup_name = "{}{}".format(remote_setup, tf_setup_name_sufix)
-            logging.info("Using full setup name: {}".format(tf_setup_name))
-            tf = Terraform(working_dir=remote_setup)
-            return_code, stdout, stderr = tf.init(capture_output=True)
-            return_code, stdout, stderr = tf.refresh()
-            tf_output = tf.output()
-            server_private_ip = tf_output["server_private_ip"]["value"][0]
-            server_public_ip = tf_output["server_public_ip"]["value"][0]
-            client_private_ip = tf_output["client_private_ip"]["value"][0]
-            client_public_ip = tf_output["client_public_ip"]["value"][0]
-            if (
-                server_private_ip is not None
-                or server_public_ip is not None
-                or client_private_ip is not None
-                or client_public_ip is not None
-            ):
-                logging.warning("Destroying previous setup")
-                tf.destroy()
-            return_code, stdout, stderr = tf.apply(
-                skip_plan=True,
-                capture_output=False,
-                refresh=True,
-                var={
-                    "github_sha": tf_github_sha,
-                    "github_actor": tf_github_actor,
-                    "setup_name": tf_setup_name,
-                    "github_repo": tf_github_repo,
-                    "redis_module": tf_redis_module,
-                },
-            )
-            tf_output = tf.output()
-            server_private_ip = tf_output["server_private_ip"]["value"][0]
-            server_public_ip = tf_output["server_public_ip"]["value"][0]
-            server_plaintext_port = 6379
-            client_private_ip = tf_output["client_private_ip"]["value"][0]
-            client_public_ip = tf_output["client_public_ip"]["value"][0]
+        if "ci" in benchmark_config and "terraform" in benchmark_config["ci"]:
+            for remote_setup in benchmark_config["ci"]["terraform"]:
+                # setup Infra
+                logging.info(
+                    "Deploying test defined in {} on AWS using {}".format(f, remote_setup)
+                )
+                tf_setup_name = "{}{}".format(remote_setup, tf_setup_name_sufix)
+                logging.info("Using full setup name: {}".format(tf_setup_name))
+                tf = Terraform(working_dir=remote_setup)
+                (
+                    return_code,
+                    username,
+                    server_private_ip,
+                    server_public_ip,
+                    server_plaintext_port,
+                    client_private_ip,
+                    client_public_ip,
+                ) = setupRemoteEnviroment(
+                    tf,
+                    tf_github_sha,
+                    tf_github_actor,
+                    tf_setup_name,
+                    tf_github_repo,
+                    tf_redis_module,
+                )
 
-            # copy the rdb to DB machine
-            dataset = None
-            username = "ubuntu"
-            remote_dataset_file = "/tmp/dump.rdb"
-            remote_module_file = "/tmp/redisgraph.so"
-            private_key = "/home/filipe/.ssh/perf-cto-joint-tasks.pem"
-            for k in benchmark_config["dbconfig"]:
-                if "dataset" in k:
-                    dataset = k["dataset"]
-            if dataset is not None:
-                copyFileToRemoteSetup(
+                # setup RedisGraph
+                spinUpRemoteRedis(
+                    benchmark_config,
                     server_public_ip,
                     username,
                     private_key,
-                    dataset,
+                    local_module_file,
+                    remote_module_file,
                     remote_dataset_file,
                 )
 
-            # copy the module to the DB machine
-            copyFileToRemoteSetup(
-                server_public_ip,
-                username,
-                private_key,
-                local_module_file,
-                remote_module_file,
-            )
-            executeRemoteCommands(
-                server_public_ip,
-                username,
-                private_key,
-                ["chmod 755 {}".format(remote_module_file)],
-            )
-
-            # start redis-server
-            commands = [
-                "redis-server --dir /tmp/ --daemonize yes --protected-mode no --loadmodule {}".format(
-                    remote_module_file
+                # setup the benchmark
+                setupRemoteBenchmark(
+                    client_public_ip, username, private_key, redisbenchmark_go_link
                 )
-            ]
-            executeRemoteCommands(server_public_ip, username, private_key, commands)
 
-            # setup the benchmark
-            commands = [
-                "wget {} -q -O /tmp/redisgraph-benchmark-go".format(
-                    redisbenchmark_go_link
-                ),
-                "chmod 755 /tmp/redisgraph-benchmark-go",
-            ]
-            executeRemoteCommands(client_public_ip, username, private_key, commands)
+                # run the benchmark
+                runRemoteBenchmark(
+                    client_public_ip, username, private_key,server_private_ip, server_plaintext_port, benchmark_config, remote_results_file, local_results_file
+                )
 
-            # run the benchmark
-            queries_str = ""
-            for k in benchmark_config["queries"]:
-                query = k["q"]
-                queries_str += ' -query "{}"'.format(query)
-                if "ratio" in k:
-                    queries_str += " -query-ratio {}".format(k["ratio"])
-
-            for k in benchmark_config["clientconfig"]:
-                if "graph" in k:
-                    queries_str += " -graph-key {}".format(k["graph"])
-                if "clients" in k:
-                    queries_str += " -c {}".format(k["clients"])
-                if "requests" in k:
-                    queries_str += " -n {}".format(k["requests"])
-                if "rps" in k:
-                    queries_str += " -rps {}".format(k["rps"])
-            queries_str += " -h {}".format(server_private_ip)
-            queries_str += " -p {}".format(server_plaintext_port)
-            results_file = "/tmp/benchmark-result.json"
-            queries_str += " -json-out-file {}".format(results_file)
-
-            logging.info(queries_str)
-
-            logging.info("Running the benchmark")
-            commands = ["/tmp/redisgraph-benchmark-go {}".format(queries_str)]
-            executeRemoteCommands(client_public_ip, username, private_key, commands)
-
-            logging.info("Extracting the benchmark results")
-            getFileFromRemoteSetup(
-                client_public_ip,
-                username,
-                private_key,
-                "./benchmark-result.json",
-                results_file,
-            )
-
-            # tear-down
-            logging.info("Tearing down setup")
-            tf_output = tf.destroy()
-            logging.info("Tear-down completed")
+                # tear-down
+                logging.info("Tearing down setup")
+                tf_output = tf.destroy()
+                logging.info("Tear-down completed")
